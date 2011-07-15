1f544ce0ff1e00cff60b44ca4c337ad877892a2f
Beat Besmer <beat.besmer@gmail.com>
2011-07-15 13:10:01 +0200

    Gmaps4Rails: Initial structure

---
+++ Gmaps4Rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps4Rails::Application.config.secret_token = 'e4d5e80689fc24122bbf43b4f911617079cce08e3f298cecc5b7a9af3c4460f0df7fbb31f919e929616749de8d2766ccaa5c78093c02b7599a09ea3f9feec23a'
